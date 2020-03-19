11f3000458de61726d0e7ad96d5de57eb52897fb
jonas-kgomo <jonaskgmoo@gmail.com>
2020-03-19 14:32:14 +0300

    okta install phase

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import "./styles/tailwind.css";
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
 import client from './apollo';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
+  <BrowserRouter>
+    <Security
+      issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
+      redirect_uri={`${window.location.origin}/implicit/callback`}
+      client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+    >
       <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>,
-      document.getElementById('root')
-    );
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+        <Route path="/" component={App} />
+      </ApolloProvider>
+    </Security>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
+serviceWorker.unregister();
+//registerServiceWorker();
 // hot reload module
 if (module.hot) module.hot.accept();
